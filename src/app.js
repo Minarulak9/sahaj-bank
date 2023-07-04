ff6e2c071c9d3c7cd1de316691820fdd9d04213c
minarul <minarulh34@gmail.com>
2023-07-04 06:57:04 +0530

    responsive done

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 let options = {
-  threshold: 0.90,
+  threshold: 0.9,
 };
-let nav = document.querySelector('nav')
+let nav = document.querySelector("nav");
 let observer = new IntersectionObserver(navChange, options);
 function navChange(entries) {
   entries.forEach((entry) => {
-      if(entry.isIntersecting) {
-        nav.classList.remove('active')
-    }else{
-          nav.classList.add('active')
-
-      }
+    if (entry.isIntersecting) {
+      nav.classList.remove("active");
+    } else {
+      nav.classList.add("active");
+    }
   });
 }
-const header = document.querySelector('header')
+const header = document.querySelector("header");
 observer.observe(header);
+
+// nav toggler
+const toggler = document.querySelector(".toggler");
+const menu = document.querySelector('.menu')
+toggler.addEventListener('click',(e)=>{
+  console.log(e.target);
+  toggler.classList.toggle('active')
+  menu.classList.toggle('active')
+})
